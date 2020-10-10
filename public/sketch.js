3b5c901597234f4a666758950b8e5b4846b87249
Tetsu Kondo <tetsu.kondo@int.t-kougei.ac.jp>
2020-10-10 09:21:11 +0900

    updated by glitch code
    updated by glitch code/

--- public/sketch.js
+++ public/sketch.js
@@ -2,21 +2,29 @@
 var socket;
 
 function setup() {
-  createCanvas(400, 400);
+  // createCanvas(windowWidth,windowHeight);
+  
+     createCanvas(windowWidth,windowHeight);
+  
+  
  // strokeWeight(2);
-  background(237, 34, 93);
+  background(0);
 
-  socket = io.connect('http://localhost:3000');
-
+  //socket = io.connect('http://localhost:3000');
+  
+socket = io.connect('https://tetsukondo.glitch.me');
+  
   socket.on('mouse',
   // When we receive data
   function(data) {
     console.log("Got: " + data.x + " " + data.y);
-    // Draw a blue circle
-    fill(0,0,255);
-    noStroke();
-    ellipse(data.x, data.y, 20, 20);
+    console.log("Got: " + data.px + " " + data.py);
+        
+  stroke(209,98,226);
+  strokeWeight(9);
+  line(data.x, data.y,data.px,data.py); 
   }
+            
 );
 
 }
@@ -30,22 +38,32 @@ function draw() {
 function mouseDragged() {
   // Draw some white circles
   fill(255);
-  noStroke();
-  ellipse(mouseX,mouseY,20,20);
+ // noStroke();
+  stroke(52,167,225);
+  strokeWeight(9);
+  line(mouseX,mouseY,pmouseX,pmouseY);
+ // ellipse(mouseX,mouseY,20,20);
+  
   // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
+  sendmouse(mouseX,mouseY,pmouseX,pmouseY);
+  
 }
 
 
 // Function for sending to the socket
-function sendmouse(xpos, ypos) {
+function sendmouse(xpos, ypos, pxpos, pypos) {
   // We are sending!
   console.log("sendmouse: " + xpos + " " + ypos);
+    console.log("sendmouse: " + pxpos + " " + pypos);
+
   
   // Make a little object with  and y
   var data = {
     x: xpos,
-    y: ypos
+    y: ypos,
+    px : pxpos,
+    py : pypos
+    
   };
 
   // Send that object to the socket
